8ab48451d9a3ad36193f42b413ab334f872dc8d5
Роман <romanze1706@gmail.com>
2022-11-03 12:55:20 +0300

    Задача-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        int age =3;
+        if (age >= 18)
+        {System.out.println("Поздравляю с совершеннолетием.");}
+
+    if (age < 18)
+        {System.out.println("вам стоит немного подождать. Вы ещё не достигли совершеннолетияю");}
+
 
     }
 }
