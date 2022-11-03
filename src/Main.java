0fadc2e99109239a4a70ac548df47d2b11c650f8
Роман <romanze1706@gmail.com>
2022-11-03 13:39:52 +0300

    Задача-2

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,11 @@ public class Main {
     if (age < 18)
         {System.out.println("вам стоит немного подождать. Вы ещё не достигли совершеннолетияю");}
 
+    int people = 19;
+    if (people >=7 && people <18) {System.out.println("Ребёнок ходит в школу");}
+    if (people >=18 && people <24) {System.out.println("человек уже закончил школу и может отправляться в университет.");}
+    if (people >=24) {System.out.println("человек окончил университет и ему пора искать первую работу");}
+
 
     }
 }
